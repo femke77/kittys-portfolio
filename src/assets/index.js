165e91515943f056baf928b6fcd1db868634f884
femke77 <megan.meyers.388@gmail.com>
2023-10-30 18:55:25 -0700

    project added and portfolio examples coded

---
+++ src/assets/index.js
@@ -0,0 +1,2 @@
+export {default as cute} from './projects/cute.jpg'
+export {default as cute2} from './projects/cute2.jpg'
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,8 +1,34 @@
-function Portfolio () {
+import Project from "./Project";
 
-    return (
-      <div className="">Portfolio section goes here</div>
-    )
+function Portfolio() {
+  // fill in the required data, image file should be in the assets/projects folder and name should match the "default as " statment in index.js
+  const projects = [
+    {
+      name: "project 1 test",
+      description: "HTML/JS/CSS",
+      link: "https://heroku.com",
+      repo: "https://github.com/femke77",
+      image: "cute",
+    },
+    {
+      name: "project 2 test",
+      description: "NRG Stack",
+      link: "https://heroku.com/",
+      repo: "https://github.com/femke77",
+      image: "cute2",
+    },
+  ];
+
+  // for each project, use the Project component to build a project
+  return (
+    <div>
+      <div className="d-flex">
+        {projects.map((project) => (
+          <Project project={project} key={"project-" + project.name} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Portfolio;

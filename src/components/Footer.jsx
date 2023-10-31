5fc2f2232ad7410efb30236cba6cff491c1581b1
femke77 <megan.meyers.388@gmail.com>
2023-10-30 20:26:57 -0700

    adjusted footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,16 @@
 function Footer() {
 
     return(      
+       
+      
+        
         <footer class="relative bg-blueGray-200 pt-8 pb-6">
           <div class="container mx-auto px-4">
             <div class="flex flex-wrap text-left lg:text-left">
               <div class="w-full lg:w-6/12 px-4">
-                <h4 class="text-2xl fonat-semibold text-blueGray-700">Let's keep in touch!</h4>
+                <h4 class="text-3xl fonat-semibold text-blueGray-700">Let's keep in touch!</h4>
                 <h5 class="text-lg mt-0 mb-2 text-blueGray-600">
-                  Find me on any of these platforms, I am a busy cat, but I will respond 1-2 business days.
+                  Find me on any of these platforms, I am a very busy cat, but I respond 1-2 business days.
                 </h5>
                 <div class="mt-6 lg:mb-0 mb-6">
                   <button class="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
@@ -23,7 +26,8 @@ function Footer() {
                   <div class="w-full lg:w-4/12 px-4 ml-auto">
                     <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Useful Links</span>
                     <ul class="list-unstyled">
-                   
+
+              
                       <li>
                         <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://blog.creative-tim.com?ref=njs-profile">Blog</a>
                       </li>
@@ -31,6 +35,7 @@ function Footer() {
                         <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.github.com/creativetimofficial?ref=njs-profile">Github</a>
                       </li>
 
+                
                     </ul>
                   </div>
                   <div class="w-full lg:w-4/12 px-4">
@@ -45,6 +50,7 @@ function Footer() {
                       <li>
                         <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/privacy?ref=njs-profile">Privacy Policy</a>
                       </li>
+
                     </ul>
                   </div>
                 </div>

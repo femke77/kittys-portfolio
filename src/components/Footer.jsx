b467fa02db2dcc614d7ea5910e063139c5c9d512
femke77 <megan.meyers.388@gmail.com>
2023-10-30 19:27:38 -0700

    adjusted footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,13 @@
 function Footer() {
 
-    return(
-      
-     
-        
+    return(      
         <footer class="relative bg-blueGray-200 pt-8 pb-6">
           <div class="container mx-auto px-4">
             <div class="flex flex-wrap text-left lg:text-left">
               <div class="w-full lg:w-6/12 px-4">
                 <h4 class="text-2xl fonat-semibold text-blueGray-700">Let's keep in touch!</h4>
                 <h5 class="text-lg mt-0 mb-2 text-blueGray-600">
-                  Find me on any of these platforms, we respond 1-2 business days.
+                  Find me on any of these platforms, I am a busy cat, but I will respond 1-2 business days.
                 </h5>
                 <div class="mt-6 lg:mb-0 mb-6">
                   <button class="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">

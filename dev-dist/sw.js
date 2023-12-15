868c3fce17f8e13e8a4236dccc525cd168ea0337
femke77 <megan.meyers.388@gmail.com>
2023-12-14 19:21:25 -0800

    added pwa status to about page

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f4a68738'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6ncm7dsnn3"
+    "revision": "0.4fmv9irt2qo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,18 @@ function About() {
         coding for many years. I know I don't look that old, but I'm paw-somely
         ageless. I love to code projects for my fellow kitties, so check out my
         work in the portfolio section.
-      </div>
+          <br></br>
+          <br></br>
+        Exciting update: This portfolio is now a PWA application! The entire app 
+        will pre-cache, work offline, is installable and is overall more amazing than ever!
+        Check out my service worker, my cache, and see how I work offline with your chrome
+        dev tools!
+       </div>
+         <p className="about mx-4 my-4">
+       
+      </p>
+   
+     
     </div>
   );
 }

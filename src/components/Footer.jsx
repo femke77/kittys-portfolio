d2eb8db1f8964daea998d4726b8917d6051c0330
femke77 <megan.meyers.388@gmail.com>
2024-02-12 15:25:57 -0800

    trying the anchor tags

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -58,6 +58,7 @@ function Footer() {
             </div>
             <hr className="my-6 border-blueGray-300"/>
             <div className="flex flex-wrap items-center md:justify-between justify-center">
+              <a href="https://www.github.com/femke77">See my github (test)</a>
               <div className="w-full md:w-4/12 px-4 mx-auto text-center">
                 <div className="text-sm text-blueGray-500 font-semibold py-1">
                   Copyright © <span id="get-current-year">2021</span>

828bf1ea66d4fb350ffb4ed5dfb68a1dc3018e96
femke77 <megan.meyers.388@gmail.com>
2024-05-21 15:54:04 -0700

    added nicer cards to the portfolio section

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-a9fe0588'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.v9s1q92q68g"
+    "revision": "0.u3kn3cettco"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,26 +1,53 @@
-import * as projects from '../assets'
+import * as projects from "../assets";
 
 function Project({ project }) {
-    const { name, repo, link, description, image } = project;
-  
-    return (
-      <div className="p-3">
+  const { name, repo, link, description, image } = project;
+
+  return (
+    <>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg m-3">
         <img
+           className="w-64 h-48 object-cover"
           src={projects[image]}
           alt={name}
-          className="p-img"
         />
-        <div className="">
-          <h1>
-            <a href={link}>{name}</a>{' '}
-            <a href={repo}>
-              <i className="fab fa-github"></i>
-            </a>
-          </h1>
-          <p>{description}</p>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{name}</div>
+          <p className="text-gray-700 text-base">
+            {description}
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          <a href={link}>{name}</a>{' '}
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          <a href={repo}>
+             <i className="fab fa-github"></i>
+          </a>
+          </span>
+         
         </div>
       </div>
-    );
-  }
-  
-  export default Project;
+    </>
+  );
+}
+
+export default Project;
+
+// <div className="p-3">
+//       <img
+//         src={projects[image]}
+//         alt={name}
+//         className="p-img"
+//       />
+//       <div className="">
+//         <h1>
+//           <a href={link}>{name}</a>{' '}
+//           <a href={repo}>
+//             <i className="fab fa-github"></i>
+//           </a>
+//         </h1>
+//         <p>{description}</p>
+//       </div>
+//     </div>
